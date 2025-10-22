756b9e17dbd617012328782041d75a871e35167e
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-10-22 15:32:06 +0100

    add feed_animal func and finish task

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -7,36 +11,43 @@ class Animal:
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self) -> int:
+    def feed(self) -> int | None:
         if self.is_hungry:
             return_number = self.appetite
+            print(f"Eating {return_number} food points...")
             self.appetite -= self.appetite
             self.is_hungry = False
             return return_number
-        else:
+        elif not self.is_hungry:
             return 0
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name=name, appetite = 3, is_hungry=is_hungry)
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True) -> None:
+        super().__init__(name=name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
 
+
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True) -> None:
         super().__init__(name=name, appetite=7, is_hungry=is_hungry)
 
-    def bring_slippers(self):
+    def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
 
-if __name__ == "__main__":
-    dog = Dog("Dog")
-    dog.print_name()  # "Hello, I'm Dog"
-    dog.feed()  # "Eating 7 food points"
-
-    dog2 = Dog("Dog", False)
-    print(dog2.feed())  # 0
-    dog2.bring_slippers()
+def feed_animals(animals: list[Animal]) -> None:
+    sum_of_food_points = 0
+    for animal in animals:
+        if animal.is_hungry:
+            sum_of_food_points += animal.appetite
+            animal.feed()
+    return sum_of_food_points
